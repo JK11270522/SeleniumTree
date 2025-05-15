62e95b0c40b1a7c7bf4a445cbb792d9e30cf1175
H-I-K <143682830+JK11270522@users.noreply.github.com>
2025-05-15 21:22:57 +0800

    Add files via upload

---
+++ LOGIC/pyautogui0.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+
+# pyautogui.click(694, 890)
+# time.sleep(2)
+# pyautogui.scroll(-15000)
+# time.sleep(2)
+# pyautogui.click(684, 333)
+# time.sleep(2)
+
+pyautogui.click(685, 421)
+time.sleep(1.5)
+# 進去之後
+# pyautogui.click(803, 581)
+pyautogui.click(1816, 77)
+time.sleep(1.5)
+pyautogui.click(585, 281)
+time.sleep(1)
